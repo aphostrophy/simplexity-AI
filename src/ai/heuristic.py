07d525c612278846ce3bda9db9ca028c73788894
Jesson Yo <Aphostrophy@gmail.com>
2021-10-07 11:00:02 +0700

    modified run.bat added auto break for anytime algorithm

---
+++ src/ai/heuristic.py
@@ -0,0 +1,8 @@
+from src.model.state import State
+
+class Heuristic:
+  def __init__(self) -> None:
+      pass
+
+  def static_eval(state : State):
+    print(state.board.board)
--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -1,8 +1,11 @@
 import random
+import multiprocessing
 from time import time
 
 from src.constant import ShapeConstant
 from src.model import State
+
+from src.ai.heuristic import Heuristic;
 
 from typing import Tuple, List
 
@@ -14,6 +17,34 @@ class Minimax:
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
 
-        best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+        # best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+
+        pdm = ProgressiveDeepeningMinimax(state,n_player)
+
+        best_movement = pdm.result_after(thinking_time)
 
         return best_movement
+
+
+class ProgressiveDeepeningMinimax:
+  def __init__(self, state: State, n_player: int):
+    self.state = state
+    self.n_player = n_player
+    self.result = (random.randint(0, self.state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
+
+  def result_after(self,seconds):
+    p = multiprocessing.Process(target=self.best_movement, name="best_movement", args=())
+    p.start()
+
+    p.join(seconds)
+
+    return self.result
+
+  def infinite_while(self):
+    i = True
+    while(True):
+      i != i
+
+  def best_movement(self):
+    current_best_movement = (random.randint(0, self.state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+    self.result = current_best_movement
