775d9a6531d4035253bbaa6133b7390375d9f50d
DenilsenAxel <denilsenaxel@gmail.com>
2021-10-09 00:05:04 +0800

    add local search

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -1,10 +1,18 @@
 import random
+import multiprocessing
 from time import time
+from src.utility import is_win
+from src.utility import choose_move, choose_heuristic, other_shape, place, unplace
+from src.ai.heuristic import heuristic
 
 from src.constant import ShapeConstant
 from src.model import State
 
 from typing import Tuple, List
+
+
+def sortPossibleMoves(moves_score):
+    return moves_score[2][0] + moves_score[2][1]
 
 
 class LocalSearch:
@@ -14,6 +22,159 @@ class LocalSearch:
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
 
-        best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
+        lsmm = LocalSearchMinMaxing(state, n_player)
+
+        best_movement = lsmm.result_after(thinking_time)  # minimax algorithm
+
+        print(f'BEST MOVE PLAYER {n_player+1}', best_movement)
 
         return best_movement
+
+
+class LocalSearchMinMaxing:
+    def __init__(self, state: State, n_player: int):
+        self.state = state
+        self.n_player = n_player
+        self.result_col = multiprocessing.Value(
+            'i', random.randint(0, self.state.board.col))
+        self.result_shape = multiprocessing.Value('i', 0 if random.choice(
+            [ShapeConstant.CROSS, ShapeConstant.CIRCLE]) == ShapeConstant.CROSS else 1)
+
+    def generateAllMoves(self, state: State):
+        primary_shape = self.state.players[self.n_player].shape
+        secondary_shape = other_shape(
+            self.state.players[self.n_player].shape)
+
+        possible_moves = []
+        # Insert with primary shape first
+        for col in range(7):
+            if(state.board[0, col].shape == ShapeConstant.BLANK and self.state.players[self.n_player].quota[primary_shape] > 0):
+                place(self.state, self.n_player, primary_shape, col)
+                possible_moves.append(
+                    (col, True, heuristic(self.state, self.n_player)))
+                unplace(self.state, self.n_player, primary_shape, col)
+
+        # Insert with secondary shape
+        for col in range(7):
+            if(state.board[0, col].shape == ShapeConstant.BLANK and self.state.players[self.n_player].quota[secondary_shape] > 0):
+                place(self.state, self.n_player, secondary_shape, col)
+                possible_moves.append(
+                    (col, False, heuristic(self.state, self.n_player)))
+                unplace(self.state, self.n_player, secondary_shape, col)
+
+        # Sort all possible moves
+        possible_moves.sort(key=sortPossibleMoves)
+        return possible_moves
+
+    def result_after(self, seconds):
+        p = multiprocessing.Process(
+            target=self.best_movement, name="best_movement", args=())
+        p.start()
+
+        p.join(seconds)
+
+        if(p.is_alive()):
+            p.terminate()
+
+        return (self.result_col.value, self.state.players[self.n_player].shape if self.result_shape.value == 0 else other_shape(self.state.players[self.n_player].shape))
+
+    def best_movement(self):
+        self.depth_limit = 1
+        win_found = False
+
+        # Generate all possible moves
+        possible_moves = self.generateAllMoves(self.state)
+        print(possible_moves)
+
+        while(True and not win_found):
+            (self.result_col.value, self.result_shape.value, win_found) = self.minimax(
+                maximizing=True, depth=self.depth_limit)
+            self.depth_limit += 1
+
+    def minimax(self, maximizing: bool, depth: int):
+        if(depth == 0):
+            return heuristic(self.state, self.n_player)
+
+        # possible_moves[0] = col 0 primary shape, 1 = col 0 secondary shape , 2 = col 1 primary shape, 3 = col 1 secondary shape, etc...
+        possible_moves = [None for _ in range(14)]
+
+        winning_tuple = is_win(self.state.board)
+        if(winning_tuple != None):
+            if(winning_tuple[0] == self.state.players[self.n_player].shape):
+                return (88888, 88888)
+            else:
+                return (-88888, -88888)
+
+        # If depth == 1 then use initial move from local search
+        if(depth == 1):
+            initial_moves = self.generateAllMoves(self.state)
+            primary_shape = self.state.players[self.n_player].shape
+            secondary_shape = other_shape(
+                self.state.players[self.n_player].shape)
+            for move in initial_moves:
+                if(move[1]):  # primary shape
+                    place(self.state, self.n_player, primary_shape, move[0])
+                    possible_moves[move[0] *
+                                   2] = self.minimax(False, depth-1)
+                    unplace(self.state, self.n_player, primary_shape, move[0])
+                else:
+                    place(self.state, self.n_player, secondary_shape, move[0])
+                    possible_moves[move[0]*2 +
+                                   1] = self.minimax(False, depth-1)
+                    unplace(self.state, self.n_player,
+                            secondary_shape, move[0])
+
+        else:  # Depth > 1 then minmaxing
+            for col in range(7):
+                if(self.state.board[0, col].shape == ShapeConstant.BLANK):
+                    if(maximizing):
+                        primary_shape = self.state.players[self.n_player].shape
+                        secondary_shape = other_shape(
+                            self.state.players[self.n_player].shape)
+
+                        # Check for primary shape
+                        if(self.state.players[self.n_player].quota[primary_shape] > 0):
+                            place(self.state, self.n_player,
+                                  primary_shape, col)
+                            possible_moves[col *
+                                           2] = self.minimax(False, depth-1)
+                            unplace(self.state, self.n_player,
+                                    primary_shape, col)
+
+                        # Check for secondary shape
+                        if(self.state.players[self.n_player].quota[secondary_shape] > 0):
+                            place(self.state, self.n_player,
+                                  secondary_shape, col)
+                            possible_moves[col*2 +
+                                           1] = self.minimax(False, depth-1)
+                            unplace(self.state, self.n_player,
+                                    secondary_shape, col)
+                    else:
+                        primary_shape = self.state.players[(
+                            self.n_player + 1) % 2].shape
+                        secondary_shape = other_shape(
+                            self.state.players[(self.n_player + 1) % 2].shape)
+                        # Check for primary shape
+                        if(self.state.players[(self.n_player + 1) % 2].quota[primary_shape] > 0):
+                            place(self.state, (self.n_player + 1) %
+                                  2, primary_shape, col)
+                            possible_moves[col *
+                                           2] = self.minimax(True, depth-1)
+                            unplace(self.state, (self.n_player + 1) %
+                                    2, primary_shape, col)
+
+                        # Check for secondary shape
+                        if(self.state.players[(self.n_player + 1) % 2].quota[secondary_shape] > 0):
+                            place(self.state, (self.n_player + 1) %
+                                  2, secondary_shape, col)
+                            possible_moves[col*2 +
+                                           1] = self.minimax(True, depth-1)
+                            unplace(self.state, (self.n_player + 1) %
+                                    2, secondary_shape, col)
+
+        if(depth == self.depth_limit):  # Pasti Maximizing
+            # print(f"POSSIBLE MOVES DEPTH {depth}", possible_moves)
+            return choose_move(possible_moves)
+
+        # Standard Case
+        return choose_heuristic(possible_moves, maximizing)
--- src/mechanic/game.py
+++ src/mechanic/game.py
@@ -1,7 +1,7 @@
 import pickle
 from time import time
 
-from src.ai import Minimax
+from src.ai import Minimax, LocalSearch
 from src.model import Board, Player, State, Config
 from src.constant import ShapeConstant, GameConstant, Path
 from src.utility import is_out, is_win, is_full, place, place_debug
@@ -23,6 +23,7 @@ class Game:
         __is_valid -> Check if input is valid
         __placement -> Placement phase for player or bot
     """
+
     def __init__(self, config: Config):
         print(config)
         self.config = config
@@ -44,8 +45,8 @@ class Game:
         if self.config.game_type == GameConstant.BVB:
             if not self.config.is_dump:
                 # You can change model used here
-                model1 = Minimax()
-                model2 = Minimax()
+                model1 = LocalSearch()
+                model2 = LocalSearch()
             else:
                 # Don't change this
                 model1 = pickle.load(open(Path.BVB_P1, "rb"))
@@ -102,7 +103,6 @@ class Game:
                 )
                 print(f'Runtime: {time() - start}')
 
-            
             if self.__is_valid(choosen_col, choosen_shape):
                 break
 
